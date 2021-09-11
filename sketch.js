896f834275c788910712badf730f19697469ecd9
vivekbeniwaljs <76515013+vivekbeniwaljs@users.noreply.github.com>
2021-09-11 12:11:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var car,wall;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+car = createSprite(200,200,50,50);
+wall = createSprite(1200,200,60,100);
+
+  car.velocityX = 9;
+
+ speed=random(25,31);
+ weight=random(400,1500);
+
+}
+
+function draw() {
+  background(000000);
+  
+
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+    car.velocityX=0;
+    var deformation=0.5 * weight * speed* speed/22509;
+    if(deformation>180)
+    {
+      car.shapeColor=color("yellow");
+      wall.shapeColor=color("blue");
+    }
+    if(deformation<180 && deformation>100)
+    {
+      car.shapeColor=color("green");
+      wall.shapeColor=color("blue");
+    }
+    if(deformation<100)
+    {
+      car.shapeColor=color("red");
+      wall.shapeColor=color("blue");
+    }
+  }
+
+  drawSprites();
+  
+}
+//by vivek beniwal
